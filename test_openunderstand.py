6d52b5aef73f05b6426139e79dfcd806a54a15a4
zhra <@zahrahabibolah10@gmail.c0m>
2024-02-08 01:17:32 +0330

    change the create_createby_g9.py and the test_openunderstand.py and the test_run.py

--- test_openunderstand.py
+++ test_openunderstand.py
@@ -5,12 +5,13 @@ from os.path import join
 sys.path.append(join(getcwd(), "openunderstand"))
 sys.path.append(join(getcwd(), "openunderstand", "oudb"))
 sys.path.append(join(getcwd(), "openunderstand", "utils"))
+
 from openunderstand.ounderstand.openunderstand import *
 
 start_parsing(
     repo_address=join(getcwd(), "benchmark", "JSON"),
     db_address=getcwd(),
-    db_name="mydb.udb",
+    db_name="JSON.udb",
     engine_core="Python3",
-    log_address=join(getcwd(), "a.log"),
-)
+    log_address=join(getcwd(), "app.log")
+    )
--- test_run.py
+++ test_run.py
@@ -1,29 +1,71 @@
 import sys
+import os
 from os import getcwd
 from os.path import join
-
+from collections import Counter
 sys.path.append(join(getcwd(), "openunderstand"))
 sys.path.append(join(getcwd(), "openunderstand", "oudb"))
 sys.path.append(join(getcwd(), "openunderstand", "utils"))
-sys.path.append(join(getcwd(), "openunderstand", "metrics"))
 import openunderstand.ounderstand as und
 
-_db = und.open("/home/y/Desktop/iust/OpenUnderstand/mydb.udb")
+#_db = und.open("C:\\Users\\black\\OpenUnderstand\\JSON.udb")
+#dbents = _db.ents()
 
-# und_all_results = {}
-# for ent in _db.ents("Class"):
-#     ent_name = ent.name()
-#     print(ent.metric(["CountDeclMethodAll"]))
-#     all_methods = ent.metric(["CountDeclMethodAll"]).get("CountDeclMethodAll", 0)
-#     und_all_results[ent_name] = all_methods
+os.add_dll_directory("C:\\Program Files\\Scitools\\bin\\pc-win64")
+sys.path.append("C:\\Program Files\\Scitools\\bin\\pc-win64\\Python")
 
-und_all_results = {}
-for ent in _db.ents("Class"):
-    ent_name = ent.name()
-    print(ent.metric(["CountDeclClassVariable"]))
-    all_methods = ent.metric(["CountDeclClassVariable"]).get(
-        "CountDeclClassVariable", 0
+
+def udb_open(address: str, refrence: str) -> (dict[str, dict], int):
+    import understand
+    db = understand.open(address)
+    dbents = db.ents()
+
+    print(f"-------------understand-------------")
+    counter = 0
+    refs_dict = {}
+    for ents in dbents:
+        for ref in ents.refs(refrence):
+            counter += 1
+            if (filename := ref.file().longname()) in refs_dict:
+                refs_dict[filename].update({ref.line(): ref.ent().longname()})
+            else:
+                refs_dict[filename] = {ref.line(): ref.ent().longname()}
+    return refs_dict, counter
+
+
+def oudb_open(address: str, refrence: str) -> (dict[str, dict], int):
+    _db = und.open(address)
+    dbents = _db.ents()
+
+    print(f"-------------open understand-------------")
+    counter = 0
+    refs_dict = {}
+    for ents in dbents:
+        for ref in ents.refs(refrence):
+            counter += 1
+            if (filename := ref.file().longname()) in refs_dict:
+                refs_dict[filename].update({ref.line(): ref.ent().longname()})
+            else:
+                refs_dict[filename] = {ref.line(): ref.ent().longname()}
+    return refs_dict, counter
+
+
+if __name__ == "__main__":
+    refrerence = "Create"
+    understand_refs, understand_counter = udb_open(
+        address="C:\\Users\\black\\OpenUnderstand\\benchmark\\JSON\\JSON.udb",
+        refrence=refrerence,
     )
-    und_all_results[ent_name] = all_methods
-
-print(und_all_results)
+    openund_refs, openund_counter = oudb_open(
+        address="C:\\Users\\black\\OpenUnderstand\\JSON.udb",
+        refrence=refrerence,
+    )
+    print(f"openund_counter: {openund_counter}")
+    print(f"understand_counter: {understand_counter}")
+    print("-------------")
+    for i in understand_refs:
+        if (un := understand_refs.get(i)) != (op := openund_refs.get(i)):
+            print(f"File: {i}")
+            print(f"openund: {op}")
+            print(f"understand: {un}")
+            print("-------------")
